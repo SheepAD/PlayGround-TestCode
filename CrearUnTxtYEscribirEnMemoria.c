41d90824ea11cb2aaec2aa943d1b768461b9ee55
SheepAD <kanutofett@gmail.com>
2025-04-17 18:29:17 -0400

    Create CrearUnTxtYEscribirEnMemoria.c
    Crear un txt y Escribir en la memoria reservada mediante el uso de Windows API.

---
+++ CrearUnTxtYEscribirEnMemoria.c
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <stdio.h>
+
+
+int main() {
+
+	LPCWSTR path = L"C:\\Users\\'$User'\\Desktop\\MyLab\\nuevo.txt";
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+
+	hFile = CreateFileW(path, GENERIC_ALL, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		printf("\n[!] Error al crear el txt... \n");
+		return -1;
+	}
+
+	BOOL cHandle = CloseHandle(hFile);
+
+	if (!cHandle) {
+		printf("\n[!] Error al cerrar handle: %lu \n", GetLastError());
+		return -1;
+	}
+
+	printf("\n[+] El archivo se ha creado correctamente! \n");
+
+	PVOID pAddress = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 100);
+	CHAR* cString = "Escribiendo en memoria justo despues de haber creado un archivo...";
+
+	memcpy(pAddress, cString, strlen(cString));
+
+	BOOL hFree = HeapFree(GetProcessHeap(), 0, pAddress);
+
+	if (!hFree) {
+		printf("Error al liberar el HEAP: %lu", GetLastError());
+		return -1;
+	}
+	return 0;
